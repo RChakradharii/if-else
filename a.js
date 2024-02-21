9f4de7c0445825488bf46faa8606fb4ef16912b8
RChakradharii <160701886+RChakradharii@users.noreply.github.com>
2024-02-21 11:06:24 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,12 @@
+const input = require("readline-sync")
+
+
+let a =  input.questionInt("enter the number: ");
+let num  = a
+if (num % 2 == 0){
+console.log("even");
+} else  {
+    console.log("odd");
+}
+   
+
